27d8469577b4e90d1af407a1effdaa74afd3dbb7
arman <armandofruss@gmail.com>
2023-10-26 14:27:07 +0200

    scacchiera.xhtml ciclo array e scacchieraBean modificato

--- ExoBankJSF/src/main/java/it/exolab/exobank/bean/ScacchieraBean.java
+++ ExoBankJSF/src/main/java/it/exolab/exobank/bean/ScacchieraBean.java
@@ -1,7 +1,9 @@
 package it.exolab.exobank.bean;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -10,8 +12,44 @@ import javax.inject.Named;
 public class ScacchieraBean implements Serializable {
 	
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
 	private boolean gioca;
+	
+	private String[][] array;
+
+    public ScacchieraBean() {
+        // Initialize your 2D array with data
+        initializeArray();
+    }
+
+    private void initializeArray() {
+    	
+        array = new String[8][8];
+
+        // Populate the array with data
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                array[i][j] = "Row " + i + ", Col " + j;
+            }
+        }
+    }
+    
+    public String getCellValue(int i, int j) {
+    	String value = array[i][j];
+    	return value;
+    }
+
+    public String[][] getArray() {
+        return array;
+    }
+
+    public void setArray(String[][] array) {
+        this.array = array;
+    }
 
 	public boolean isGioca() {
 		return gioca;
@@ -25,8 +63,10 @@ public class ScacchieraBean implements Serializable {
 	public void switchGioca() {
 		gioca = !gioca;
 	}
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "ScacchieraBean [gioca=" + gioca + ", array=" + Arrays.toString(array) + "]";
+	}
 	
 }
