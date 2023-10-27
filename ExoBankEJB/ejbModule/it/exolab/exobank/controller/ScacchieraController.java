abbc5dd2e89719fd2ca1b40f2921025b6bee1016
alfredo.roma99@gmail.com <alfredo.roma99@gmail.com>
2023-10-27 14:13:02 +0200

    scacchiera controller e interface

--- ExoBankEJB/ejbModule/it/exolab/exobank/controller/ScacchieraController.java
+++ ExoBankEJB/ejbModule/it/exolab/exobank/controller/ScacchieraController.java
@@ -13,6 +13,7 @@ import it.exolab.exobank.chess.model.Alfiere;
 import it.exolab.exobank.chess.model.Pezzo;
 import it.exolab.exobank.chess.model.Scacchiera;
 import it.exolab.exobank.costanti.Costanti;
+import it.exolab.exobank.ejbinterface.ScacchieraControllerInterface;
 import it.exolab.exobank.validatore.ValidaMosseScacchi;
 
 @Stateless(name = "ScacchieraControllerInterface")
@@ -21,122 +22,169 @@ public class ScacchieraController implements ScacchieraControllerInterface {
 
 	private Scacchiera scacchieraLavoro = new Scacchiera();
 	private ValidaMosseScacchi validatoreScacchi = new ValidaMosseScacchi();
-	
-	
+
 	@Override
-	public Scacchiera scacchieraIniziale() {
-		Scacchiera scacchieraIniziale = creazioneScacchieraIniziale();
-		return scacchieraIniziale;
+	public Scacchiera scacchieraIniziale() throws Exception{
+		try {
+			Scacchiera scacchieraIniziale = creazioneScacchieraIniziale();
+			scacchieraLavoro = scacchieraIniziale;
+			return scacchieraIniziale;
+		} catch (Exception e) {
+			// Gestisci l'eccezione qui
+			e.printStackTrace();
+			throw new Exception("Contatta assistenza non ti si è creata la scacchiera iniziale");
+		}
 	}
 
 	@Override
-	public boolean mossaConsentita(Pezzo pezzo) {
-	    Pezzo[][] griglia = scacchieraLavoro.getGriglia();
+	public Scacchiera mossaConsentita(Pezzo pezzo) throws Exception {
+		boolean ritorno = true;
+		Pezzo[][] griglia = scacchieraLavoro.getGriglia();
 
-	    // Trova il pezzo specifico sulla scacchiera usando l'ID
-	    Pezzo pezzoSpecifico = (Pezzo) findPezzoById(pezzo, griglia);
+		try {
+			// Trova il pezzo specifico sulla scacchiera usando l'ID
+			Pezzo pezzoSpecifico = (Pezzo) findPezzoById(pezzo, griglia);
 
-	    if (pezzoSpecifico == null) {
-	        return false; // Nessun pezzo con l'ID specifico è stato trovato.
-	    }
+			if (pezzoSpecifico == null) {
+				ritorno = false; // Nessun pezzo con l'ID specifico è stato trovato.
+			}
 
-	    Integer xPartenza = pezzoSpecifico.getPosizioneX();
-	    Integer yPartenza = pezzoSpecifico.getPosizioneY();
-	    Integer xDestinazione = pezzo.getPosizioneX();
-	    Integer yDestinazione = pezzo.getPosizioneY();
-	    // Verifica se la destinazione è all'interno della scacchiera
-	    if (xDestinazione < 0 || xDestinazione >= griglia.length || yDestinazione < 0 || yDestinazione >= griglia[0].length) {
-	        return false; // La destinazione è fuori dalla scacchiera.
-	    }
+			Integer xPartenza = pezzoSpecifico.getPosizioneX();
+			Integer yPartenza = pezzoSpecifico.getPosizioneY();
+			Integer xDestinazione = pezzo.getPosizioneX();
+			Integer yDestinazione = pezzo.getPosizioneY();
+			// Verifica se la destinazione è all'interno della scacchiera
+			if (xDestinazione < 0 || xDestinazione >= griglia.length || yDestinazione < 0 || yDestinazione >= griglia[0].length) {
+				ritorno = false; // La destinazione è fuori dalla scacchiera.
+			}
 
-	    // Verifica se la destinazione è la stessa posizione di partenza
-	    if (xPartenza.equals(xDestinazione) && yPartenza.equals(yDestinazione)) {
-	        return false; // La destinazione è la stessa posizione di partenza.
-	    }
+			// Verifica se la destinazione è la stessa posizione di partenza
+			if (xPartenza.equals(xDestinazione) && yPartenza.equals(yDestinazione)) {
+				ritorno = false; // La destinazione è la stessa posizione di partenza.
+			}
 
-	    Pezzo pezzoDestinazione = griglia[xDestinazione][yDestinazione];
+			Pezzo pezzoDestinazione = griglia[xDestinazione][yDestinazione];
 
-	    // Verifica se il pezzo di destinazione è dello stesso colore del pezzo in movimento
-	    if (pezzoDestinazione != null && pezzoDestinazione.getColore().equals(pezzoSpecifico.getColore())) {
-	        return false; // Il pezzo di destinazione è dello stesso colore del pezzo in movimento.
-	    }
+			// Verifica se il pezzo di destinazione è dello stesso colore del pezzo in movimento
+			if (pezzoDestinazione != null && pezzoDestinazione.getColore().equals(pezzoSpecifico.getColore())) {
+				ritorno = false; // Il pezzo di destinazione è dello stesso colore del pezzo in movimento.
+			}
 
-	    // Utilizza il validatoreScacchi per verificare la validità della mossa
-	    return validatoreScacchi.mossaValida(pezzoSpecifico, xPartenza, yPartenza, xDestinazione, yDestinazione, griglia);
+			if (ritorno) {
+				// Utilizza il validatoreScacchi per verificare la validità della mossa
+				if (validatoreScacchi.mossaValida(pezzoSpecifico, xPartenza, yPartenza, xDestinazione, yDestinazione, griglia)) {
+					if (null != griglia[xDestinazione][yDestinazione]) {
+						griglia[xDestinazione][yDestinazione].setEsiste(false); // setto esiste a false cosi dico che il pezzo non esiste piu sulla scacchiera
+						griglia[xDestinazione][yDestinazione] = null; // setto a null quella cella della griglia cosi da dire che il pezzo è stato mangiato
+						// Aggiorna la scacchiera con la nuova posizione
+						griglia[xPartenza][yPartenza] = null; // Rimuovi il pezzo dalla posizione precedente
+						griglia[xDestinazione][yDestinazione] = pezzoSpecifico; // Sposta il pezzo nella nuova posizione
+						pezzoSpecifico.setPosizioneX(xDestinazione); // Aggiorna le coordinate del pezzo
+						pezzoSpecifico.setPosizioneY(yDestinazione);
+
+					} else {
+						// Aggiorna la scacchiera con la nuova posizione
+						griglia[xPartenza][yPartenza] = null; // Rimuovi il pezzo dalla posizione precedente
+						griglia[xDestinazione][yDestinazione] = pezzoSpecifico; // Sposta il pezzo nella nuova posizione
+						pezzoSpecifico.setPosizioneX(xDestinazione); // Aggiorna le coordinate del pezzo
+						pezzoSpecifico.setPosizioneY(yDestinazione);
+
+					}
+				}
+				scacchieraLavoro.setScacchiera(griglia);
+			}else {
+				throw new Exception("Mossa non consentita");
+			}
+			return scacchieraLavoro;
+		} catch (Exception e) {
+			// Gestisci l'eccezione qui
+			e.printStackTrace();
+			throw new Exception(null != e.getMessage() ? e.getMessage() : "Contatta assistenza si è rotto qualcosa");
+		}
+
 	}
 
 	// Metodo per creare la scacchiera iniziale
-	private Scacchiera creazioneScacchieraIniziale() {
-		Pezzo[][] griglia = new Pezzo[8][8];
-		int idPezzo = 1;
+	private Scacchiera creazioneScacchieraIniziale() throws Exception {
+		try {
+			Pezzo[][] griglia = new Pezzo[8][8];
+			int idPezzo = 1;
 
-		// Inizializza la prima riga (0) con i pezzi iniziali (torre, cavallo, alfiere, re, regina, alfiere, cavallo, torre)
-		griglia[0][0] = new Torre(Costanti.BIANCO, 0, 0, idPezzo++, true);
-		griglia[0][1] = new Cavallo(Costanti.BIANCO, 0, 1, idPezzo++, true);
-		griglia[0][2] = new Alfiere(Costanti.BIANCO, 0, 2, idPezzo++, true);
-		griglia[0][3] = new Re(Costanti.BIANCO, 0, 3, idPezzo++, true);
-		griglia[0][4] = new Regina(Costanti.BIANCO, 0, 4, idPezzo++, true);
-		griglia[0][5] = new Alfiere(Costanti.BIANCO, 0, 5, idPezzo++, true);
-		griglia[0][6] = new Cavallo(Costanti.BIANCO, 0, 6, idPezzo++, true);
-		griglia[0][7] = new Torre(Costanti.BIANCO, 0, 7, idPezzo++, true);
+			// Inizializza la prima riga (0) con i pezzi iniziali (torre, cavallo, alfiere, re, regina, alfiere, cavallo, torre)
+			griglia[0][0] = new Torre(Costanti.BIANCO, 0, 0, idPezzo++, true);
+			griglia[0][1] = new Cavallo(Costanti.BIANCO, 0, 1, idPezzo++, true);
+			griglia[0][2] = new Alfiere(Costanti.BIANCO, 0, 2, idPezzo++, true);
+			griglia[0][3] = new Re(Costanti.BIANCO, 0, 3, idPezzo++, true);
+			griglia[0][4] = new Regina(Costanti.BIANCO, 0, 4, idPezzo++, true);
+			griglia[0][5] = new Alfiere(Costanti.BIANCO, 0, 5, idPezzo++, true);
+			griglia[0][6] = new Cavallo(Costanti.BIANCO, 0, 6, idPezzo++, true);
+			griglia[0][7] = new Torre(Costanti.BIANCO, 0, 7, idPezzo++, true);
 
-		// Inizializza la seconda riga (1) con i pedoni bianchi
-		for (int colonna = 0; colonna < 8; colonna++) {
-			griglia[1][colonna] = new Pedone(Costanti.BIANCO, 1, colonna, idPezzo++, true);
-		}
-
-		// Inizializza le righe 2-5 con caselle vuote
-		for (int riga = 2; riga < 6; riga++) {
+			// Inizializza la seconda riga (1) con i pedoni bianchi
 			for (int colonna = 0; colonna < 8; colonna++) {
-				griglia[riga][colonna] = null; // Casella vuota
+				griglia[1][colonna] = new Pedone(Costanti.BIANCO, 1, colonna, idPezzo++, true);
 			}
-		}
 
-		// Inizializza la sesta riga (6) con i pedoni neri
-		for (int colonna = 0; colonna < 8; colonna++) {
-			griglia[6][colonna] = new Pedone(Costanti.NERO, 6, colonna, idPezzo++, true);
-		}
-
-		// Inizializza l'ultima riga (7) con i pezzi iniziali neri (torre, cavallo, alfiere, re, regina, alfiere, cavallo, torre)
-		griglia[7][0] = new Torre(Costanti.NERO, 7, 0, idPezzo++, true);
-		griglia[7][1] = new Cavallo(Costanti.NERO, 7, 1, idPezzo++, true);
-		griglia[7][2] = new Alfiere(Costanti.NERO, 7, 2, idPezzo++, true);
-		griglia[7][3] = new Re(Costanti.NERO, 7, 3, idPezzo++, true);
-		griglia[7][4] = new Regina(Costanti.NERO, 7, 4, idPezzo++, true);
-		griglia[7][5] = new Alfiere(Costanti.NERO, 7, 5, idPezzo++, true);
-		griglia[7][6] = new Cavallo(Costanti.NERO, 7, 6, idPezzo++, true);
-		griglia[7][7] = new Torre(Costanti.NERO, 7, 7, idPezzo++, true);
-
-		Scacchiera scacchiera = new Scacchiera();
-		scacchiera.setScacchiera(griglia);
-		scacchieraLavoro.setScacchiera(griglia);
-		return scacchiera;
-	}
-
-	public Object findPezzoById(Pezzo pezzo, Pezzo[][] griglia) {
-		Integer idPezzo = pezzo.getId();
-		for (int x = 0; x < 8; x++) {
-			for (int y = 0; y < 8; y++) {
-				if (null != griglia[x][y] && griglia[x][y].getId() == idPezzo) {
-					if (griglia[x][y] instanceof Torre) {
-						return (Torre) griglia[x][y];
-					} else if (griglia[x][y] instanceof Alfiere) {
-						return (Alfiere) griglia[x][y];
-					}  else if (griglia[x][y] instanceof Re) {
-						return (Re) griglia[x][y];
-					}  else if (griglia[x][y] instanceof Regina) {
-						return (Regina) griglia[x][y];
-					}  else if (griglia[x][y] instanceof Cavallo) {
-						return (Cavallo) griglia[x][y];
-					}  else if (griglia[x][y] instanceof Pedone) {
-						return (Pedone) griglia[x][y];
-					} 
+			// Inizializza le righe 2-5 con caselle vuote
+			for (int riga = 2; riga < 6; riga++) {
+				for (int colonna = 0; colonna < 8; colonna++) {
+					griglia[riga][colonna] = null; // Casella vuota
 				}
 			}
+
+			// Inizializza la sesta riga (6) con i pedoni neri
+			for (int colonna = 0; colonna < 8; colonna++) {
+				griglia[6][colonna] = new Pedone(Costanti.NERO, 6, colonna, idPezzo++, true);
+			}
+
+			// Inizializza l'ultima riga (7) con i pezzi iniziali neri (torre, cavallo, alfiere, re, regina, alfiere, cavallo, torre)
+			griglia[7][0] = new Torre(Costanti.NERO, 7, 0, idPezzo++, true);
+			griglia[7][1] = new Cavallo(Costanti.NERO, 7, 1, idPezzo++, true);
+			griglia[7][2] = new Alfiere(Costanti.NERO, 7, 2, idPezzo++, true);
+			griglia[7][3] = new Re(Costanti.NERO, 7, 3, idPezzo++, true);
+			griglia[7][4] = new Regina(Costanti.NERO, 7, 4, idPezzo++, true);
+			griglia[7][5] = new Alfiere(Costanti.NERO, 7, 5, idPezzo++, true);
+			griglia[7][6] = new Cavallo(Costanti.NERO, 7, 6, idPezzo++, true);
+			griglia[7][7] = new Torre(Costanti.NERO, 7, 7, idPezzo++, true);
+
+			Scacchiera scacchiera = new Scacchiera();
+			scacchiera.setScacchiera(griglia);
+			scacchieraLavoro.setScacchiera(griglia);
+			return scacchiera;
+		} catch (Exception e) {
+			// Gestisci l'eccezione qui
+			e.printStackTrace();
+			throw new Exception("Griglia non creata contatta assistenza"); // Restituisci un valore adeguato in caso di errore
 		}
-		return null; // Se nessun pezzo con l'ID specifico è stato trovato
-	} 
+	}
 
-
-	
+	public Object findPezzoById(Pezzo pezzo, Pezzo[][] griglia) throws Exception {
+		try {
+			Integer idPezzo = pezzo.getId();
+			for (int x = 0; x < 8; x++) {
+				for (int y = 0; y < 8; y++) {
+					if (null != griglia[x][y] && griglia[x][y].getId() == idPezzo) {
+						if (griglia[x][y] instanceof Torre) {
+							return (Torre) griglia[x][y];
+						} else if (griglia[x][y] instanceof Alfiere) {
+							return (Alfiere) griglia[x][y];
+						} else if (griglia[x][y] instanceof Re) {
+							return (Re) griglia[x][y];
+						} else if (griglia[x][y] instanceof Regina) {
+							return (Regina) griglia[x][y];
+						} else if (griglia[x][y] instanceof Cavallo) {
+							return (Cavallo) griglia[x][y];
+						} else if (griglia[x][y] instanceof Pedone) {
+							return (Pedone) griglia[x][y];
+						}
+					}
+				}
+			}
+			return null; // Se nessun pezzo con l'ID specifico è stato trovato
+		} catch (Exception e) {
+			// Gestisci l'eccezione qui
+			e.printStackTrace();
+			throw new Exception("Pezzo non trovato"); // Restituisci un valore adeguato in caso di errore
+		}
+	}
 }
