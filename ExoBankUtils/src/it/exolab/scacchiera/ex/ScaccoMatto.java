94161c5b9004b1e93767f6d46f76ffc3af062597
arman <armandofruss@gmail.com>
2023-11-06 14:24:18 +0100

    aggiunta eccezione per scacco matto

---
+++ ExoBankUtils/src/it/exolab/scacchiera/ex/ScaccoMatto.java
@@ -0,0 +1,14 @@
+package it.exolab.scacchiera.ex;
+
+public class ScaccoMatto extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3354652918227400078L;
+	
+	public ScaccoMatto(String message) {
+		super(message);
+	}
+
+}
