a12d9dacfaeeddc0b26891b574fcc7de2e3dc50a
arman <armandofruss@gmail.com>
2023-11-06 13:54:49 +0100

    aggiunta eccezione scacco

---
+++ ExoBankUtils/src/it/exolab/scacchiera/ex/Scacco.java
@@ -0,0 +1,14 @@
+package it.exolab.scacchiera.ex;
+
+public class Scacco extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5484444536641822954L;
+	
+	public Scacco(String message) {
+		super(message);
+	}
+
+}
