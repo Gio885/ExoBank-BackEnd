13b50b22f2b602cf6f04f6902ff5b528cdea6045
arman <armandofruss@gmail.com>
2023-11-06 12:54:03 +0100

    aggiunto package per errori specifici

---
+++ ExoBankUtils/src/it/exolab/scacchiera/ex/MossaNonConsentita.java
@@ -0,0 +1,14 @@
+package it.exolab.scacchiera.ex;
+
+public class MossaNonConsentita extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6119979575612283256L;
+	
+	public MossaNonConsentita(String message) {
+		super(message);
+	}
+
+}
